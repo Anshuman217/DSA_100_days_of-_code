2a5c37c5911851d87833873569bf34389fb0aa6c
Anshuman Sahu <116194676+Anshuman217@users.noreply.github.com>
2026-02-02 22:31:00 +0530

    Add files via upload

---
+++ Day2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int pos;
+    scanf("%d", &pos);
+
+    int idx = pos - 1;
+
+    for (int i = idx; i < n - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        printf("%d", arr[i]);
+        if (i < n - 2) {
+            printf(" ");
+        }
+    }
+
+    return 0;
+}
