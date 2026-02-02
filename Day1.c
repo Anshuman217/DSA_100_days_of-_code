a3942139ab9c27840a09936ec0dfeea08d155982
Anshuman Sahu <116194676+Anshuman217@users.noreply.github.com>
2026-02-02 22:29:04 +0530

    Day1.c

--- Day1.c
+++ Day1.c
@@ -4,24 +4,27 @@ int main() {
     int n;
     scanf("%d", &n);
 
-    int arr[n];
+    int arr[n + 1];
 
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
-    int pos;
+    int pos, x;
     scanf("%d", &pos);
+    scanf("%d", &x);
 
     int idx = pos - 1;
 
-    for (int i = idx; i < n - 1; i++) {
-        arr[i] = arr[i + 1];
+    for (int i = n; i > idx; i--) {
+        arr[i] = arr[i - 1];
     }
 
-    for (int i = 0; i < n - 1; i++) {
+    arr[idx] = x;
+
+    for (int i = 0; i < n + 1; i++) {
         printf("%d", arr[i]);
-        if (i < n - 2) {
+        if (i < n) {
             printf(" ");
         }
     }
